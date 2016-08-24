5634f7df0c6c24693b894adfb2b429b574bbb06a
papicella <pas.apicella@tpg.com.au>
2016-08-24 15:08:42 +1000

    Set reasonable system preferences

--- src/test/java/com/pivotal/pcf/mysqlweb/PivotalMySqlWebApplicationTests.java
+++ src/test/java/com/pivotal/pcf/mysqlweb/PivotalMySqlWebApplicationTests.java
@@ -1,10 +1,14 @@
 package com.pivotal.pcf.mysqlweb;
 
+import com.pivotal.pcf.mysqlweb.rest.VersionRest;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = PivotalMySqlWebApplication.class)
