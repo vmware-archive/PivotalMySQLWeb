fdf26565a98d93c7f6d98bf6c6985810247bd94a
papicella <pas.apicella@tpg.com.au>
2016-08-25 22:28:36 +1000

    Added basic TEST to verify Login page displays

--- src/test/java/com/pivotal/pcf/mysqlweb/PivotalMySqlWebApplicationTests.java
+++ src/test/java/com/pivotal/pcf/mysqlweb/PivotalMySqlWebApplicationTests.java
@@ -1,14 +1,26 @@
 package com.pivotal.pcf.mysqlweb;
 
+import com.pivotal.pcf.mysqlweb.controller.LoginController;
 import com.pivotal.pcf.mysqlweb.rest.VersionRest;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = PivotalMySqlWebApplication.class)
@@ -19,4 +31,29 @@ public class PivotalMySqlWebApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Autowired private WebApplicationContext ctx;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setUp() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+	}
+
+	@Test public void testLoginPage() throws Exception {
+		mockMvc.perform(get("/").accept(MediaType.TEXT_HTML))
+				.andDo(print())
+				.andExpect(status().isOk());
+	}
+
+	@Configuration
+	public static class TestConfiguration
+	{
+
+		@Bean
+		public LoginController loginController() {
+			return new LoginController();
+		}
+
+	}
 }
