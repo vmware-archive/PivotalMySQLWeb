1e3ac16ef77f72d61a325025e64a2e435e61f2ba
papicella <pas.apicella@tpg.com.au>
2017-05-18 21:57:30 +1000

    Added view of database variables with ability to filter by variable name/value

---
+++ src/main/java/com/pivotal/pcf/mysqlweb/controller/VariableController.java
@@ -0,0 +1,66 @@
+/*
+PivotalMySQLWeb
+
+Copyright (c) 2017-Present Pivotal Software, Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+package com.pivotal.pcf.mysqlweb.controller;
+
+import com.pivotal.pcf.mysqlweb.utils.ConnectionManager;
+import com.pivotal.pcf.mysqlweb.utils.QueryUtil;
+import com.pivotal.pcf.mysqlweb.utils.Utils;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.sql.Connection;
+
+@Controller
+public class VariableController
+{
+    protected static Logger logger = Logger.getLogger(VariableController.class);
+
+    @RequestMapping(value = "/variables", method = RequestMethod.GET)
+    public String databaseVariables
+            (Model model, HttpServletResponse response, HttpServletRequest request, HttpSession session) throws Exception
+    {
+        if (Utils.verifyConnection(response, session))
+        {
+            logger.info("user_key is null OR Connection stale so new Login required");
+            return null;
+        }
+
+        logger.info("Received request to show database variables");
+
+        javax.servlet.jsp.jstl.sql.Result dbVariables;
+
+        // retrieve connection
+        ConnectionManager cm = ConnectionManager.getInstance();
+        Connection conn = cm.getConnection(session.getId());
+
+        dbVariables = QueryUtil.runQuery(conn,
+                                        "SHOW VARIABLES",
+                                        -1);
+
+        model.addAttribute("dbVariables", dbVariables);
+
+        return "variables";
+    }
+
+}
