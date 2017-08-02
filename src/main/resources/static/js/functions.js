d524f21a310aeb465cf1a0d12a42d3270fbb30b6
papicella <pas.apicella@tpg.com.au>
2017-08-02 14:48:58 +1000

    Ensured DROP sql statements trigger a schema refresh for the UI

--- src/main/resources/static/js/functions.js
+++ src/main/resources/static/js/functions.js
@@ -1,10 +1,11 @@
 /**
- Copyright (C) MARCH-2014 Pivotal Software, Inc.
+ PivotalMySQLWeb
 
- All rights reserved. This program and the accompanying materials
- are made available under the terms of the under the Apache License,
- Version 2.0 (the "License”); you may not use this file except in compliance
- with the License. You may obtain a copy of the License at
+ Copyright (c) 2017-Present Pivotal Software, Inc. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
  http://www.apache.org/licenses/LICENSE-2.0
 
@@ -13,11 +14,6 @@
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  See the License for the specific language governing permissions and
  limitations under the License.
-
- * marks all rows and selects its first checkbox inside the given element
- * the given element is usaly a table or a div containing the table or tables
- *
- * @param    container    DOM element
  */
 function markAllRows( container_id ) {
     var rows = document.getElementById(container_id).getElementsByTagName('tr');
